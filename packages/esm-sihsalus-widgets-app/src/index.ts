7b684525b8f8e8fe9bbfc264dd97f0c566981e70
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-06-25 21:35:45 -0500

    renombrado paquete

--- packages/esm-sihsalus-widgets-app/src/index.ts
+++ packages/esm-sihsalus-widgets-app/src/index.ts
@@ -97,7 +97,7 @@ import { ObstetricHistoryBase } from './ui/obstetric-history-widget';
 // ================================================================================
 export const importTranslation = require.context('../translations', false, /.json$/, 'lazy');
 
-const moduleName = '@pucp-gidis-hiisc/esm-sihsalus-aditionals-app';
+const moduleName = '@pucp-gidis-hiisc/esm-sihsalus-widgets-app';
 const options = {
   featureName: 'patient-clinical-view-app',
   moduleName,
