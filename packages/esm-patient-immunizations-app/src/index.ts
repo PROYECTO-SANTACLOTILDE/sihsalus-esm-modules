e32220f4b84dab13ae45fa94404f821d128e1ebd
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-07-10 20:28:35 -0500

    Refactor code structure for improved readability and maintainability

--- packages/esm-patient-immunizations-app/src/index.ts
+++ packages/esm-patient-immunizations-app/src/index.ts
@@ -5,7 +5,7 @@ import { dashboardMeta } from './dashboard.meta';
 import immunizationsOverviewComponent from './immunizations/immunizations-overview.component';
 import immunizationsDetailedSummaryComponent from './immunizations/immunizations-detailed-summary.component';
 
-const moduleName = '@openmrs/esm-patient-immunizations-app';
+const moduleName = '@pucp-gidis-hiisc/esm-patient-immunizations-app';
 
 const options = {
   featureName: 'patient-immunizations',
