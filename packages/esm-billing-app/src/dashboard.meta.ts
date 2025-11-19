dd0e53ca17f799267b169f4a67031fc15b9a413b
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2025-11-19 15:36:04 -0500

    eliminar billing

--- packages/esm-billing-app/src/dashboard.meta.ts
+++
@@ -1,8 +0,0 @@
-export const dashboardMeta = {
-  slot: 'patient-chart-billing-dashboard-slot',
-  columns: 1,
-  title: 'Billing history',
-  hideDashboardTitle: true,
-  icon: '',
-  path: 'Billing history',
-};
