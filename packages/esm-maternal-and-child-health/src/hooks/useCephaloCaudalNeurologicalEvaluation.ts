f134782a2e3993df397b9ce2c065d8932dd5416f
Gonzalo <gonzalo.galvezc@pucp.edu.pe>
2026-02-23 04:26:34 -0500

    feat: eliminar UUID de tipo de encuentro obsoleto en useCephaloCaudalNeurologicalEvaluation

--- packages/esm-maternal-and-child-health/src/hooks/useCephaloCaudalNeurologicalEvaluation.ts
+++ packages/esm-maternal-and-child-health/src/hooks/useCephaloCaudalNeurologicalEvaluation.ts
@@ -4,11 +4,9 @@ import { openmrsFetch, restBaseUrl, useConfig } from '@openmrs/esm-framework';
 import type { ConfigObject } from '../config-schema'; // Adjust the import path as needed
 import { encounterRepresentation } from '../utils/constants'; // Adjust the import path as needed
 
-export const cephaloCaudalNeurologicalEvaluationEncounterUuid = '2v3w4x5y-2234-5678-9101-abcdefghij22'; // Encounter type UUID from JSON
-
 export function useCephaloCaudalNeurologicalEvaluation(patientUuid: string) {
   const config = useConfig() as ConfigObject;
-  const url = `${restBaseUrl}/encounter?patient=${patientUuid}&encounterType=${config.encounterTypes.postnatalControl || cephaloCaudalNeurologicalEvaluationEncounterUuid}&v=${encounterRepresentation}`;
+  const url = `${restBaseUrl}/encounter?patient=${patientUuid}&encounterType=${config.encounterTypes.postnatalControl}&v=${encounterRepresentation}`;
 
   const { data, error, isLoading, isValidating, mutate } = useSWR<{ data: { results: Array<Encounter> } }, Error>(
     url,
